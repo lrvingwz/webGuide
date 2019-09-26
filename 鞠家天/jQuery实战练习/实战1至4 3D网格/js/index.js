279e55f4e7816d5b9ac9204e836e95002d34e2e9
baseBird <765677689@qq.com>
2019-09-26 11:22:02 +0800

    0926

--- 鞠家天/jQuery实战练习/实战1至4 3D网格/js/index.js
+++ 鞠家天/jQuery实战练习/实战1至4 3D网格/js/index.js
@@ -10,17 +10,18 @@ $(function () {
     var tX,tY,tZ;
     tX=400;tY=400;tZ=700;
     var infmtArr=[
-        {"title":"Cs3","name":"鞠家天","date":"2019.09.16","show-t":"你猜猜这是啥玩意儿","href":"demo/demo1"},
-        {"title":"Cs3","name":"鞠家天","date":"2019.09.16","show-t":"你猜猜这是啥玩意儿","href":"demo/demo1"},
-        {"title":"Cs3","name":"鞠家天","date":"2019.09.16","show-t":"你猜猜这是啥玩意儿","href":"demo/demo1"},
-        {"title":"Cs3","name":"鞠家天","date":"2019.09.16","show-t":"你猜猜这是啥玩意儿","href":"demo/demo1"},
-        {"title":"Cs3","name":"鞠家天","date":"2019.09.16","show-t":"你猜猜这是啥玩意儿","href":"demo/demo1"},
-        {"title":"Cs3","name":"鞠家天","date":"2019.09.16","show-t":"你猜猜这是啥玩意儿","href":"demo/demo1"},
-        {"title":"Cs3","name":"鞠家天","date":"2019.09.16","show-t":"你猜猜这是啥玩意儿","href":"demo/demo1"},
-        {"title":"Js", "name":"鞠家天","date":"2019.06.29","show-t":"你猜猜这是啥玩意儿","href":"demo/demo1"},
-        {"title":"H",  "name":"鞠家天","date":"2019.06.06","show-t":"你猜猜这是啥玩意儿","href":"demo/demo1"},
-        {"title":"Abc","name":"鞠家天","date":"2019.09.16","show-t":"你猜猜这是啥玩意儿","href":"demo/demo1"}
+        {"title":"Cs3","name":"鞠家天","date":"2019.09.16","show-t":"你猜猜这是啥玩意儿","href":"demo/demo1","dec":0},
+        {"title":"Cs3","name":"鞠家天","date":"2019.09.16","show-t":"你猜猜这是啥玩意儿","href":"demo/demo1","dec":0},
+        {"title":"Cs3","name":"鞠家天","date":"2019.09.16","show-t":"你猜猜这是啥玩意儿","href":"demo/demo1","dec":0},
+        {"title":"Cs3","name":"鞠家天","date":"2019.09.16","show-t":"你猜猜这是啥玩意儿","href":"demo/demo1","dec":0},
+        {"title":"Cs3","name":"鞠家天","date":"2019.09.16","show-t":"你猜猜这是啥玩意儿","href":"demo/demo1","dec":0},
+        {"title":"Cs3","name":"鞠家天","date":"2019.09.16","show-t":"你猜猜这是啥玩意儿","href":"demo/demo1","dec":0},
+        {"title":"Cs3","name":"鞠家天","date":"2019.09.16","show-t":"你猜猜这是啥玩意儿","href":"demo/demo1","dec":0},
+        {"title":"Js", "name":"鞠家天","date":"2019.06.29","show-t":"你猜猜这是啥玩意儿","href":"demo/demo1","dec":0},
+        {"title":"H",  "name":"鞠家天","date":"2019.06.06","show-t":"你猜猜这是啥玩意儿","href":"demo/demo1","dec":0},
+        {"title":"Abc","name":"鞠家天","date":"2019.09.16","show-t":"你猜猜这是啥玩意儿","href":"demo/demo1","dec":0}
     ];
+    var direction=["balabalabalabalabalabalabala"];
     init();
 function init(){
     for(var i=0;i<liNum;i++){
@@ -42,6 +43,8 @@ function init(){
             "transform":"translate3d("+x+"px,"+y+"px,"+z+"px)"
         });
         theLi.appendTo("#wrap");
+        theLi.attr({"showT":infmt["show-t"],"href":infmt["href"],"dec":infmt["dec"]});
+
     }
     var timeOut=setTimeout(function () {
         Grid();
@@ -115,7 +118,7 @@ function init(){
         },13);
     });
     $("#menu li").each(function () {
-        $(this).click(function () {
+        $(this).click(function (ev) {
             var index=$(this).index();
             switch (index){
                 case 0:
@@ -131,7 +134,7 @@ function init(){
                     Grid();
                     break;
             }
-
+            ev.stopPropagation();
         });
     });
     function Grid(){
@@ -213,4 +216,53 @@ function init(){
             tArr.splice(rNum,1);
         }
     }
+
+    $("#wrap li").click(function (ev) {
+            $("#show").fadeIn(500).css({
+                "display":"block",
+                "transition": "all 0.5s",
+                "transform":"scale(1)"
+            });
+            $("#show .title").text($(this).attr("showT"));
+            $("#show .imgbox img").attr("src",$(this).attr("href")+"/index.png");
+            $("#show .compere").text("主持人："+$(this).find("p:eq(2)").text());
+            $("#show .dec").text(direction[$(this).attr("dec")]);
+            $("#show").attr("hre",$(this).attr("href"));
+        ev.stopPropagation();
+    });
+    $(document).click(function (ev) {
+        $("#show").fadeOut(500, function () {
+            $(this).css({
+                "transform":"scale(1.5) rotateY(0deg)",
+                "display":"none"
+            });
+        }).css({"transform":"scale(0) rotateY(180deg)"});
+        ev.stopPropagation();
+    });
+    $("#show").click(function (ev) {
+        var $show=$("#show");
+        $("#minwindow").find("iframe").attr("src",$show.attr("hre")+"/index.html");
+        $("#body").css({
+            "transition":"left 1s",
+            "left":"-100%"
+        });
+        $("#minwindow").css({
+            "left":"0",
+            "transition":"left 1s"
+        });
+        ev.stopPropagation();
+    });
+    $("#back").click(function (ev) {
+        $("#show").css({
+            "transform":"scale(1.5) rotateY(0deg)",
+            "display":"none"
+        });
+        $("#body").css({
+            "left":"0"
+        });
+        $("#minwindow").css({
+            "left":"100%"
+        });
+        ev.stopPropagation();
+    });
 });
